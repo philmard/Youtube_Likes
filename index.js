da02300202ab39aed2d916810241e133d1dd26f7
philmard <philipmardir@gmail.com>
2024-11-03 20:44:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const { google } = require("googleapis");
+require("dotenv").config();
+const CronJob = require("cron").CronJob;
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+const updateVideo = async () => {
+  oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+  // YouTube client
+  const youtube = google.youtube({
+    version: "v3",
+    auth: oauth2Client,
+  });
+
+  try {
+    // Get video
+    const result = await youtube.videos.list({
+      id: "1VDokWmVXs8", // <-- ID of video
+      part: "statistics,snippet",
+    });
+
+    if (result.data.items.length > 0) {
+      const stats = result.data.items[0].statistics;
+
+      await youtube.videos.update({
+        part: "snippet",
+        requestBody: {
+          id: "1VDokWmVXs8",
+          snippet: {
+            title: `This video has ${stats.viewCount} views`,
+            categoryId: 28,
+          },
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateEvery8Mins = new CronJob("*/8 * * * * *", async () => {
+  updateVideo();
+});
+
+updateEvery8Mins.start();
